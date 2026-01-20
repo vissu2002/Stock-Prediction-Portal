3ce741a90b3ffb6f04c9b539f53b391ea8e91b3b
vismitha <vismithasl21@gmail.com>
2026-01-20 12:06:36 +0530

    registration page

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import Button from './Button'
+import Header from './Header'
+import Footer from './Footer'
 
 const Main = () => {
   return (
     <>
-       <div className='container'>
-           <div className='p-5 text-center bg-light-dark rounded'>
-                <h1 className='text-light'>Stock Prediction Portal</h1>
-                <p className='text-light lead'>Stock Prediction Portal is a full-stack web application designed to analyze market data and generate stock price predictions using machine learning models.
+    
+    <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+             <h1 className='text-light'>Stock Prediction Portal</h1>
+             <p className='text-light lead'>Stock Prediction Portal is a full-stack web application designed to analyze market data and generate stock price predictions using machine learning models.
 The platform offers real-time stock visualization, historical analysis, and AI-based prediction tools to help users make more informed investment decisions.</p>
-                <Button text="Login" className="btn-outline-info" />
+             <Button text="Login" className="btn-info" />
 
-           </div>
-       </div>
+        </div>
+    </div>
+
+    
     </>
   )
 }
